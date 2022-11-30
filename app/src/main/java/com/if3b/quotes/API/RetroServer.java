487c39c49fbae5c07fada34952621663c5e7b2ae
Dissentleviathan <maghasp22@gmail.com>
2022-11-30 15:03:34 +0700

    retro

---
+++ app/src/main/java/com/if3b/quotes/API/RetroServer.java
@@ -0,0 +1,12 @@
+package com.if3b.quotes.API;
+
+import retrofit2.Retrofit;
+
+public class RetroServer {
+    private static final String baseUrl = "https://type.fit/api/";
+    private static Retrofit retro;
+
+    public static Retrofit connectRetrofit(){
+    
+    }
+}
