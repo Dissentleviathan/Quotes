13ab5cfb954e274b859cba7fcf22ae282cbebcd2
Dissentleviathan <maghasp22@gmail.com>
2022-12-05 11:20:44 +0700

    3

--- app/src/main/java/com/if3b/quotes/Activity/MainActivity.java
+++ app/src/main/java/com/if3b/quotes/Activity/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.if3b.quotes.API.APIRequestData;
 import com.if3b.quotes.API.RetroServer;
+import com.if3b.quotes.Adapter.AdapterQuotes;
 import com.if3b.quotes.Model.QuotesModel;
 import com.if3b.quotes.R;
 
@@ -23,6 +24,8 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
     private RecyclerView rvQuotes;
     private ProgressBar pbQuotes;
+    private List<QuotesModel> listQuotes;
+    private AdapterQuotes adQuotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,12 +47,16 @@ public class MainActivity extends AppCompatActivity {
         retrieveProcess.enqueue(new Callback<List<QuotesModel>>() {
             @Override
             public void onResponse(Call<List<QuotesModel>> call, Response<List<QuotesModel>> response) {
-
+                listQuotes = response.body();
+                adQuotes = new AdapterQuotes(listQuotes, MainActivity.this);
+                rvQuotes.setAdapter(adQuotes);
+                pbQuotes.setVisibility(View.GONE);
             }
 
             @Override
             public void onFailure(Call<List<QuotesModel>> call, Throwable t) {
                 Toast.makeText(MainActivity.this, "There was an error on the server and the request could not be completed.", Toast.LENGTH_SHORT).show();
+                pbQuotes.setVisibility(View.GONE);
             }
         });
     }
