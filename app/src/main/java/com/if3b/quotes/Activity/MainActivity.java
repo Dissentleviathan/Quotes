4bfdff9763b682a43294cd1feee252351b141ca9
Dissentleviathan <maghasp22@gmail.com>
2022-12-05 10:52:19 +0700

    sukses 2

--- app/src/main/java/com/if3b/quotes/Activity/MainActivity.java
+++ app/src/main/java/com/if3b/quotes/Activity/MainActivity.java
@@ -1,16 +1,56 @@
 package com.if3b.quotes.Activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.if3b.quotes.API.APIRequestData;
+import com.if3b.quotes.API.RetroServer;
+import com.if3b.quotes.Model.QuotesModel;
 import com.if3b.quotes.R;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvQuotes;
+    private ProgressBar pbQuotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvQuotes = findViewById(R.id.rv_quotes);
+        pbQuotes = findViewById(R.id.pb_quotes);
+
+        rvQuotes.setLayoutManager(new LinearLayoutManager(this));
+        retrieveQuotes();
+    }
+    private void retrieveQuotes(){
+        pbQuotes.setVisibility(View.VISIBLE);
+        APIRequestData ARD = RetroServer.connectRetrofit().create(APIRequestData.class);
+        Call<List<QuotesModel>> retrieveProcess = ARD.requestData();
+
+
+        retrieveProcess.enqueue(new Callback<List<QuotesModel>>() {
+            @Override
+            public void onResponse(Call<List<QuotesModel>> call, Response<List<QuotesModel>> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<List<QuotesModel>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "There was an error on the server and the request could not be completed.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
